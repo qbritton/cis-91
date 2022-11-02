643e43e7ca45589fe8b587ee6ef18d4210e34cf2
Quinn Britton <qbritton1@gmail.com>
2022-11-02 03:48:19 +0000

    lab10fix

--- lab10/main.tf
+++ lab10/main.tf
@@ -106,6 +106,11 @@ resource "google_compute_disk" "blockstorage" {
   size = "100"
 }
 
+resource "google_storage_bucket" "quinns-cloud-bucket" {
+  name = "quinns-cloud-bucket"
+  location = "US"
+}
+
 output "external-ip" {
   value = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
 }
