d4731f49fe54eec9287b151359c9754eb0284241
Quinn Britton <qbritton1@gmail.com>
2022-11-03 23:20:16 +0000

    dokuwiki

---
+++ dokuwiki/main.tf
@@ -0,0 +1,66 @@
+variable "credentials_file" { 
+  default = "/home/qui6130/sincere-signal-361922-f506e6f9b1d8.json" 
+}
+
+variable "project" {
+  default = "sincere-signal-361922"
+}
+
+variable "region" {
+  default = "us-central1"
+}
+
+variable "zone" {
+  default = "us-central1-c"
+}
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file(var.credentials_file)
+  region  = var.region
+  zone    = var.zone 
+  project = var.project
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "dokuwiki-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "dokuwiki-istance"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2004-lts"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_firewall" "default-firewall" {
+  name = "dokuwiki-firewall"
+  network = google_compute_network.vpc_network.name
+  allow {
+    protocol = "tcp"
+    ports = ["22", "80"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+}
+
+output "external-ip" {
+  value = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
+}
